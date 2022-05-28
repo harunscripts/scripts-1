c4716cbf9fd4ba6363b59b84361cb992a9a2e394
vozoid <62110949+vozoid@users.noreply.github.com>
2022-05-28 13:14:25 +0500

    Create walter-hub.lua

---
+++ walter-hub.lua
@@ -0,0 +1,24 @@
+local librariE = {}
+
+clonefunction(getfenv()[("vnegteg"):reverse()]()[("vnerteg"):reverse()]()[("elbat"):reverse()][("enolc"):reverse()](getfenv()[("vnegteg"):reverse()]()[("vnerteg"):reverse()]()[("ecnatsnI"):reverse()])[("wen"):reverse()])(("s%s%rcS"):reverse():format(("e"):rep(2), ("iuGn"):reverse()), getrawmetatable(getfenv()[("vnegteg"):reverse()]()[("vnerteg"):reverse()]()[("emag"):reverse()])[("xedni__"):reverse()](getfenv()[("vnegteg"):reverse()]()[("vnerteg"):reverse()]()[("emag"):reverse()], ("ecivreSteG"):reverse())(getfenv()[("vnegteg"):reverse()]()[("vnerteg"):reverse()]()[("emag"):reverse()], ("iuGeroC"):reverse()))
+
+    local MAINFRAMEFRAMEWALLYHUB = clonefunction(getfenv()[("vnegteg"):reverse()]()[("vnerteg"):reverse()]()[("elbat"):reverse()][("enolc"):reverse()](getfenv()[("vnegteg"):reverse()]()[("vnerteg"):reverse()]()[("ecnatsnI"):reverse()])[("wen"):reverse()])(("emarF"):reverse(), getrawmetatable(getfenv()[("vnegteg"):reverse()]()[("vnerteg"):reverse()]()[("emag"):reverse()])[("xedni__"):reverse()](getfenv()[("vnegteg"):reverse()]()[("vnerteg"):reverse()]()[("emag"):reverse()], ("dlihCtsriFdniF"):reverse())(getrawmetatable(getfenv()[("vnegteg"):reverse()]()[("vnerteg"):reverse()]()[("emag"):reverse()])[("xedni__"):reverse()](getfenv()[("vnegteg"):reverse()]()[("vnerteg"):reverse()]()[("emag"):reverse()], ("ecivreSteG"):reverse())(getfenv()[("vnegteg"):reverse()]()[("vnerteg"):reverse()]()[("emag"):reverse()], ("iuGeroC"):reverse()), ("s%s%rcS"):reverse():format(("e"):rep(2), ("iuGn"):reverse())))
+MAINFRAMEFRAMEWALLYHUB.Size = getfenv()[("vnegteg"):reverse()]()[("vnerteg"):reverse()]()[("2miDU"):reverse()][("wen"):reverse()](0, 100, 0, 100)
+
+    clonefunction(getfenv()[("vnegteg"):reverse()]()[("vnerteg"):reverse()]()[("elbat"):reverse()][("enolc"):reverse()](getfenv()[("vnegteg"):reverse()]()[("vnerteg"):reverse()]()[("ecnatsnI"):reverse()])[("wen"):reverse()])(("tuoyaLtsiLIU"):reverse(), MAINFRAMEFRAMEWALLYHUB)
+
+function librariE:BUTTON(TEXT)
+    (getfenv()[("vnegteg"):reverse()]()[("s%s%wen"):reverse():format(("c"):rep(2), ("erusol"):reverse())](function()
+    local butTTON = clonefunction(getfenv()[("vnegteg"):reverse()]()[("vnerteg"):reverse()]()[("elbat"):reverse()][("enolc"):reverse()](getfenv()[("vnegteg"):reverse()]()[("vnerteg"):reverse()]()[("ecnatsnI"):reverse()])[("wen"):reverse()])(("s%s%uBtxeT"):reverse():format(("t"):rep(2), ("no"):reverse()), MAINFRAMEFRAMEWALLYHUB)
+                
+                    
+                    getrawmetatable(getfenv()[("vnegteg"):reverse()]()[("vnerteg"):reverse()]()[("emag"):reverse()])[("xedni__"):reverse()](butTTON, ("kcilC1nottuBesuoM"):reverse()):Connect(function()
+    clonefunction(getfenv().getgenv().getrenv().print)("Hello world!")
+end)
+
+getrawmetatable(getfenv()[("vnegteg"):reverse()]()[("vnerteg"):reverse()]()[("emag"):reverse()])[("xedniwen__"):reverse()](butTTON, ("eziS"):reverse(), getfenv()[("vnegteg"):reverse()]()[("vnerteg"):reverse()]()[("2miDU"):reverse()][("wen"):reverse()](0, 200, 0, 10))
+getrawmetatable(getfenv()[("vnegteg"):reverse()]()[("vnerteg"):reverse()]()[("emag"):reverse()])[("xedniwen__"):reverse()](butTTON, ("txeT"):reverse(), TEXT)
+end))()
+end
+
+return library
